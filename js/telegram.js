9ea45884694f3e77ebfa6ad903f790bdb8ecd5ae
kazanskiy1191-glitch <kazanskiy1191@bk.ru>
2026-05-28 19:49:39 +0300

    Fix CORS with sendBeacon

--- js/telegram.js
+++ js/telegram.js
@@ -1,13 +1,8 @@
 const NTFY_TOPIC = 'vapeshop33';
 
 function sendTelegram(message) {
-  var url = 'https://ntfy.sh/' + NTFY_TOPIC;
   var data = message.replace(/<[^>]*>/g, '');
-
-  try {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'text/plain');
-    xhr.send(data);
-  } catch(e) {}
+  var url = 'https://ntfy.sh/' + NTFY_TOPIC;
+  var blob = new Blob([data], { type: 'text/plain' });
+  navigator.sendBeacon(url, blob);
 }
