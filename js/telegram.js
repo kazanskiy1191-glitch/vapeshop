37d3d6d48e7b474364d8d37df9c3c8c4cf472afb
kazanskiy1191-glitch <kazanskiy1191@bk.ru>
2026-05-28 19:39:13 +0300

    Switch to ntfy.sh notifications

--- js/telegram.js
+++ js/telegram.js
@@ -1,10 +1,13 @@
-const TELEGRAM_BOT_TOKEN = '8990430263:AAGQmAlhFR5JGNaQwlu9lJkRAOpPVH4UQi4';
-const TELEGRAM_CHAT_ID = '2114826236';
+const NTFY_TOPIC = 'vapeshop33';
 
 function sendTelegram(message) {
-  var url = 'https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN + '/sendMessage?chat_id=' + TELEGRAM_CHAT_ID + '&text=' + encodeURIComponent(message) + '&parse_mode=HTML&disable_web_page_preview=true';
+  var url = 'https://ntfy.sh/' + NTFY_TOPIC;
+  var data = message.replace(/<[^>]*>/g, '');
+
   try {
-    var img = new Image();
-    img.src = url;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.send(data);
   } catch(e) {}
 }
